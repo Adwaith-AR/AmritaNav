998fab78bda7be2173ac529ec12e88253a01e760
Adwaith-AR <adwaith.rakhy.office@gmail.com>
2026-09-09 14:56:10 +0530

    tools 5

--- code/Tools/app.js
+++ code/Tools/app.js
@@ -21,48 +21,57 @@ function closeMenu() {
 
 const Tools = {
           "Classroom Locator": {
+                    type:1,
                     Label: "Classroom Locator",
                     IconClass: "fa-solid fa-location-dot",
                     description: "Find any classroom instantly."
           },
           "Food Court": {
+                    type: 1,
                     Label: "Food Court",
                     IconClass: "fa-solid fa-utensils",
                     description: "Menus, locations and timings.",
                     Options: [
                               {
+                                        type: 2,
                                         label: "MandiMansil",
                                         link: "https://maps.app.goo.gl/Dr6Q8fMmTnynD9vm7?g_st=ac",
                                         distance: "120m away"
                               },
                               {
+                                        type: 2,
                                         label: "Rayan",
                                         link: "",
                                         distance: "120m away"
                               }
                               ,
                               {
+                                        type: 2,
                                         label: "Alrayan",
                                         link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac", 
                                         distance: "120m away"
                               },
                               {
+                                        type: 2,
                                         label: "Alrayan",
                                         link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac",
                                         distance: "120m away"
                               }
                               ,
                               {
+                                        type: 2,
                                         label: "Alrayan",
                                         link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac",
                                         distance: "120m away"
                               },
                               {
+                                        type: 2,
                                         label: "Alrayan",
                                         link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac",
                                         distance: "120m away"
                               },
                               {
+                                        type: 2,
                                         label: "Alrayan",
                                         link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac",
                                         distance: "120m away"
@@ -70,16 +79,19 @@ const Tools = {
                     ]
           },
           "Clubs": {
+                    type: 1,
                     Label: "Clubs",
                     IconClass: "fa-solid fa-users",
                     description: "Explore all student clubs.",
                     Options: [
                               {
+                                        type: 2,
                                         label: "Bios",
                                         link: "",
                                         distance: "120m away"
                               },
                               {
+                                        type: 2,
                                         label: "Amfoss",
                                         link: "",
                                         distance: "120m away"
@@ -88,29 +100,43 @@ const Tools = {
                     ]
           },
           "Emergency": {
+                    type: 1,
                     Label: "Emergency",
                     IconClass: "fa-solid fa-phone",
                     description: "Quick access to important contacts.",
                     Options: [
                               {
-                                        label: "Fire",
+                                        type: 3,
+                                        label: "Fire 1",
                                         link: "",
-                                        distance: "120m away"
+                                        Icon:"fa-solid fa-fire"
+                                        
                               },
                               {
-                                        label: "Amfoss",
+                                        type: 3,
+                                        label: "Fire 2",
                                         link: "",
-                                        distance: "120m away"
+                                        Icon: "fa-solid fa-fire"
+
                               },
+                              
+                              {
+                                        type: 3,
+                                        label: "Ambulance",
+                                        link: "",
+                                        Icon:"fa-solid fa-truck-medical"
+                              }
 
                     ]
           },
           "Gyms": {
+                    type: 1,
                     Label: "Gyms",
                     IconClass: "fa-solid fa-dumbbell",
                     description: "Quick access to important contacts."
           },
           "Announcements": {
+                    type: 1,
                     Label: "Announcements",
                     IconClass: "fa-solid fa-bullhorn",
                     description: "Stay updated with campus news."
@@ -119,31 +145,30 @@ const Tools = {
 
 
 Object.values(Tools).forEach(element => {
-          items.push(CardGenerator(1, element.Label, element.description, element.IconClass, ""))
+          items.push(CardGenerator(1, element.Label, element.description,"", element.IconClass, ""))
           container.innerHTML = items.join("")
 
 
 });
 
 
-
 function item_selected(ItemName) {
           items = []
           Tools[ItemName].Options.forEach(element => {
-                    items.push(CardGenerator(2, element.label, element.distance, "", element.link))
+                    items.push(CardGenerator(element.type, element.label, element.description,element.distance,element.Icon, element.link))
           })
           container.innerHTML = items.join("")
 
 
 }
-function CardGenerator(type, label, p, Icon, link) {
+function CardGenerator(type, label,description,distance, Icon, link) {
           switch (type) {
                     case 1:
                               return (`
                                         <div class="card" onclick="item_selected('${label}')">
                                                   <i class="${Icon}"></i>
                                                   <h3>${label}</h3>
-                                                  <p>${p}</p>
+                                                  <p>${description}</p>
                                         </div>
                                         `)
                               break
@@ -153,13 +178,21 @@ function CardGenerator(type, label, p, Icon, link) {
                                                   <img src="./img/${label}.png" alt="">
                                                   <div class="details">
                                                             <h3>${label}</h3>
-                                                            <p>${p}</p>
+                                                            <p>${distance}</p>
                                                             <a href="${link}">
                                                                       <button>Lets's Go</button>
                                                             </a>
                                                   </div>
                                         </div>
                                         `)
+                    case 3:
+                              return (`<a class="cardType3" href="tel:${link}">
+
+                                                  <i class="${Icon}"></i>
+                                                  <h3>${label}</h3>
+                                                  
+
+                                        </a>`)                                        
           }
 
 }
