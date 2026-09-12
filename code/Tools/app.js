584690f8c52552ef519014aa6313af47c4d7a81c
Adwaith-AR <adwaith.rakhy.office@gmail.com>
2026-09-13 02:04:43 +0530

    detailes oppening completed

--- code/Tools/app.js
+++ code/Tools/app.js
@@ -21,7 +21,7 @@ function closeMenu() {
 
 const Tools = {
           "Classroom Locator": {
-                    type:1,
+                    type: 1,
                     Label: "Classroom Locator",
                     IconClass: "fa-solid fa-location-dot",
                     description: "Find any classroom instantly."
@@ -48,7 +48,7 @@ const Tools = {
                               {
                                         type: 2,
                                         label: "Alrayan",
-                                        link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac", 
+                                        link: "https://maps.app.goo.gl/858me3E9kB4CGfCH7?g_st=ac",
                                         distance: "120m away"
                               },
                               {
@@ -109,8 +109,8 @@ const Tools = {
                                         type: 3,
                                         label: "Fire 1",
                                         link: "",
-                                        Icon:"fa-solid fa-fire"
-                                        
+                                        Icon: "fa-solid fa-fire"
+
                               },
                               {
                                         type: 3,
@@ -119,12 +119,12 @@ const Tools = {
                                         Icon: "fa-solid fa-fire"
 
                               },
-                              
+
                               {
                                         type: 3,
                                         label: "Ambulance",
                                         link: "",
-                                        Icon:"fa-solid fa-truck-medical"
+                                        Icon: "fa-solid fa-truck-medical"
                               }
 
                     ]
@@ -142,26 +142,39 @@ const Tools = {
                     description: "Stay updated with campus news."
           }
 }
+const data = {
+          "MandiMansil": {
+                    type: 4,
+                    label: "MandiMansil",
+                    link: "https://maps.app.goo.gl/Dr6Q8fMmTnynD9vm7?g_st=ac",
+                    distance: "120m away",
+                    contacts: ["+91 9544457621", "+91 8918282189", "+91 8918282189"],
+                    items: ["porita", "mandi", "chiken", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi", "kundi"],
+                    price: ["110", "200", "232", "232", "232", "232", "232", "232", "232", "232", "232", "232", "232", "232", "232"]
+          }
+}
 
 
 Object.values(Tools).forEach(element => {
-          items.push(CardGenerator(1, element.Label, element.description,"", element.IconClass, ""))
+          items.push(CardGenerator(1, element.Label, element.description, "", element.IconClass, ""))
           container.innerHTML = items.join("")
 
 
 });
 
+function closeMenu() { item_selected(localStorage.getItem('ItemName')) }
 
 function item_selected(ItemName) {
+          localStorage.setItem("ItemName", ItemName)
           items = []
           Tools[ItemName].Options.forEach(element => {
-                    items.push(CardGenerator(element.type, element.label, element.description,element.distance,element.Icon, element.link))
+                    items.push(CardGenerator(element.type, element.label, element.description, element.distance, element.Icon, element.link))
           })
           container.innerHTML = items.join("")
 
 
 }
-function CardGenerator(type, label,description,distance, Icon, link) {
+function CardGenerator(type, label, description, distance, Icon, link, items, price, details) {
           switch (type) {
                     case 1:
                               return (`
@@ -171,13 +184,14 @@ function CardGenerator(type, label,description,distance, Icon, link) {
                                                   <p>${description}</p>
                                         </div>
                                         `)
-                              break
+
                     case 2:
                               return (`
                                         <div class="card_type2" id="${label}" onclick="expand('${label}')">
                                                   <img src="./img/${label}.png" alt="">
-                                                  <div class="details">
-                                                            <h3>${label}</h3>
+                                                  <div id="${label}d" class="details">
+                                               
+                                                  <h3 id="${label}h">${label}</h3>
                                                             <p>${distance}</p>
                                                             <a href="${link}">
                                                                       <button>Lets's Go</button>
@@ -185,6 +199,7 @@ function CardGenerator(type, label,description,distance, Icon, link) {
                                                   </div>
                                         </div>
                                         `)
+
                     case 3:
                               return (`<a class="cardType3" href="tel:${link}">
 
@@ -192,20 +207,71 @@ function CardGenerator(type, label,description,distance, Icon, link) {
                                                   <h3>${label}</h3>
                                                   
 
-                                        </a>`)                                        
+                                        </a>`)
+
+                    case 4:
+                              let k, ItemList = "", contacts = ""
+                              console.log(items,price,details)
+                              for (k = 0; k < items.length; k++) {
+                                        ItemList += `
+                                                  <tr>
+                                                             <td>${items[k]}</td>
+                                                             <td>${price[k]}</td>
+                                                  </tr>`
+
+                              }
+
+                              for (k = 0; k < details.length; k++) {
+                                        contacts += `
+                                                            <a href="tel:${details[k]}">${details[k]}</a>
+                                                            `
+                              }
+                              return (`
+                                                            <h3>${label}</h3>
+                                                            <p>${distance}</p>
+                                                            <table>
+                                                                      <thead>
+                                                                               
+                                                                                <tr>
+                                                                                          <td>Item</td>
+                                                                                          <td>Price</td>
+                                                                                </tr>
+                                                                      </thead>
+                                                                      ${ItemList}
+
+                                                            </table>
+                                                            <div class="Phone">${contacts}</div>
+                                                            <a href="${link}">
+                                                                      <button>Lets's Go</button>
+                                                            </a>
+                                                            
+                                                            <button class="close"><a href=""><i class="fa-solid fa-xmark"></a></i></button>
+                                                             
+                                                  `)
+
           }
 
 }
 function expand(id) {
-          const box =document.getElementById(id)
-          if (box.style.width != "100vw") { 
-                    document.body.style="overflow:hidden"
+          const box = document.getElementById(id)
+          const details = document.getElementById(id + "d")
+          if (box.style.width != "100vw") {
+                    window.scrollTo({
+                              top: 0,
+                              behavior: 'instant'
+                    });
+                    document.body.style = "overflow:hidden;"
+                    container.style = "overflow:scroll;"
                     box.style = "filter: blur(20px);"
+
                     box.style = "filter: blur(20px);animation:expand 200ms forwards;  z-index: 99999999999999;position: absolute;"
                     setTimeout(() => {
-                              box.style = " height: 100vh; width: 100vw;filter: blur(0px); z-index: 99999999999999;position: absolute;top: 0px;  left: 0px;";
+                              details.innerHTML = CardGenerator(data[id].type, data[id].label, "", data[id].distance, "", data[id].link, data[id].items, data[id].price, data[id].contacts)
+                              box.style = "overflow:scroll; height: 100vh; width: 100vw;filter: blur(0px); z-index: 99999999999999;position: absolute;top: 0px;  left: 0px;";
+
+                              box.classList.replace('card_type2', 'card_type4');
                     }, 400);
           }
-         
-           
+
+
 }
